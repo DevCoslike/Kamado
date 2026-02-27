f6e2d31963069e43a0f7b866f74f37ac94e76184
Konstantinos Konstantinou <konstantinos.konstantinou@amdocs.com>
2026-02-27 19:13:14 +0200

    Fix linting

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css'
 import 'primereact/resources/themes/bootstrap4-light-blue/theme.css'
 
-import { PrimeReactProvider } from 'primereact/api'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import {PrimeReactProvider} from 'primereact/api'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import {Provider} from 'react-redux'
+import {BrowserRouter} from 'react-router-dom'
 
 import App from './App.tsx'
-import { store } from './store'
+import {store} from './store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
